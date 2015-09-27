99b83543934dc03e5cfe14986f0b99c043514c30
zeewii <zeewii@sina.com>
2015-09-27 19:30:07 +0800

    2015.9.27.19.30

--- connect/ssh.py
+++ connect/ssh.py
@@ -2,7 +2,7 @@
 
 #描述:使用ssh链接路由器的后台
 #作者：尹霞，曾祥卫
-import pexpect
+import pexpect,pxssh
 import datetime,os,time
 from data import data
 
@@ -72,3 +72,34 @@ def ssh_cmd2(cmd):
     except IOError,e:
         print "文件信息错误,具体信息："
         print e
+
+#输入：user-登录名,ip-登录ip,password-登录密码,command-输入命令
+#输出：输入命令返回的结果
+def ssh_cmd3(user,ip,password,command):
+    try:
+        #为ssh命令生成一个pxssh类的对象
+        child = pxssh.pxssh()
+        #利用 pxssh 类的 login 方法进行 ssh 登录，\
+        # 原始 prompt 为'$' , '#',这里加入'>'
+        child.login(ip,user,password,original_prompt='[#$>]')
+        #发送命令
+        child.sendline(command)
+        #匹配 prompt,即匹配最后出现的字符串有'#$>'
+        child.prompt()
+        result = child.before
+
+        #将执行命令的时间和结果以追加的形式保存到log.txt文件中备份文件
+        f = open('log.txt','a')
+        str1 = str(datetime.datetime.now())+' '
+        f.writelines(str1+result)
+        f.close()
+
+        # 退出 ssh session
+        child.logout()
+        # 将 prompt 前所有内容返回给函数,即命令的执行结果
+        return result
+
+    #异常打印原因并删除public key
+    except pxssh.ExceptionPxssh,e:
+        print "ssh连接失败，正在重启进程",str(e)
+        pexpect.run("rm -rf ~/.ssh")
