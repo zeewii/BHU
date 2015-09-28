22c47a36be8dde23d73e7742718ebf878147c664
zeewii <zeewii@sina.com>
2015-09-28 18:39:24 +0800

    2015.9.28

--- connect/ssh.py
+++ connect/ssh.py
@@ -2,7 +2,7 @@
 
 #描述:使用ssh链接路由器的后台
 #作者：尹霞，曾祥卫
-import pexpect,pxssh
+import pexpect,pxssh,subprocess
 import datetime,os,time
 from data import data
 
@@ -102,4 +102,34 @@ def ssh_cmd3(user,ip,password,command):
     #异常打印原因并删除public key
     except pxssh.ExceptionPxssh,e:
         print "ssh连接失败，正在重启进程",str(e)
-        pexpect.run("rm -rf ~/.ssh")
+        subprocess.call("rm -rf ~/.ssh",shell=True)
+
+#通过tcpdump抓各个接口的包
+#输入：user-登录名,ip-登录ip,password-登录密码,command-tcpdump的命令
+#输出：无
+def tcpdump_command(user,ip,password,command):
+    try:
+        #为ssh命令生成一个pxssh类的对象
+        child = pxssh.pxssh()
+        #利用 pxssh 类的 login 方法进行 ssh 登录，\
+        # 原始 prompt 为'$' , '#',这里加入'>'
+        child.login(ip,user,password,original_prompt='[#$>]')
+        #发送命令
+        child.sendline(command)
+        #抓取时间120s
+        time.sleep(120)
+        #输入Ctrl+c停止tcpdump
+        child.sendcontrol('c')
+        #匹配 prompt,即匹配最后出现的字符串有'#$>'
+        child.prompt()
+        #result = child.before
+
+        # 退出 ssh session
+        child.logout()
+        # 将 prompt 前所有内容返回给函数,即命令的执行结果
+        #return result
+
+    #异常打印原因并删除public key
+    except pxssh.ExceptionPxssh,e:
+        print "ssh连接失败，正在重启进程",str(e)
+        subprocess.call("rm -rf ~/.ssh",shell=True)
